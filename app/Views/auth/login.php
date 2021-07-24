0dedd36fd466582f5a8f11cef0e0a0cd513d7158
alifahnurh <86333191+alifahnurh@users.noreply.github.com>
2021-07-24 22:53:12 +0700

    Add files via upload

---
+++ app/Views/auth/login.php
@@ -0,0 +1,66 @@
+<?= $this->extend('auth/template/index'); ?>
+
+<?= $this->section('login'); ?>
+<div id="layoutAuthentication">
+    <div id="layoutAuthentication_content">
+        <main>
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-5">
+                        <div class="card shadow-lg border-0 rounded-lg mt-5">
+                            <div class="card-header">
+                                <h3 class="text-center font-weight-light my-4">Login</h3>
+                            </div>
+                            <?= form_open('auth/ceklogin'); ?>
+                            <div class="card-body">
+                                <div class="card-body">
+                                    <!-- pesan validasi error -->
+                                    <?php $errors = session()->getFlashdata('errors');
+                                    if (!empty($errors)) { ?>
+                                        <div class="alert alert-danger" role="alert">
+                                            <ul>
+                                                <?php foreach ($errors as $error) : ?>
+                                                    <li><?= esc($error) ?></li>
+                                                <?php endforeach ?>
+                                            </ul>
+                                        </div>
+                                    <?php  } ?>
+                                    <!-- pesan validasi berhasil -->
+                                    <?php
+                                    if (session()->getFlashdata('pesan')) {
+                                        echo '<div class="alert alert-danger" role="alert">';
+                                        echo  session()->getFlashdata('pesan');
+                                        echo '</div>';
+                                    }
+                                    ?>
+                                    <form>
+                                        <div class="form-group">
+                                            <label class="small mb-1" for="email">Email</label>
+                                            <input class="form-control py-4" id="email" type="email" name="email" aria-describedby="emailHelp" placeholder="Enter email address" />
+                                        </div>
+                                        <div class="form-group">
+                                            <label class="small mb-1" for="Password">Password</label>
+                                            <input class="form-control py-4" id="Password" name="password" type="password" placeholder="Enter password" />
+                                        </div>
+                                        <div class="form-group mt-4 mb-0"><button type="submit" class="btn btn-primary btn-block">Login</button></div>
+                                    </form>
+                                </div>
+                                <?= form_close(); ?>
+                                <div class="card-footer text-center">
+                                    <div class="small"><a href="register">Need an account? Sign up!</a></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        </main>
+    </div>
+</div>
+<script>
+    window.setTimeout(function() {
+        $(".alert").fadeTo(500, 0).slideUp(500, function() {
+            $(this).remove();
+        });
+    }, 3000);
+</script>
+<?= $this->endSection(); ?>
